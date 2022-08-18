29dfd7f7fa94eee2b50aa1c9f509c81e640c27b9
Dicky Kurniawan <89238386+kurniawandicky02@users.noreply.github.com>
2022-08-18 15:55:26 +0700

    Add files via upload

---
+++ _footer.php
@@ -0,0 +1,12 @@
+<style>
+a:hover{
+	text-decoration:none;
+}
+	</style>
+<footer class="sticky-footer">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright 2022 © <a href="https://github.com/kurniawandicky02/" target="_blank">Dicky Kurniawan</a></span>
+		</div>
+	</div>
+</footer>
